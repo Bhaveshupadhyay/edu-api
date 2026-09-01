68817dfe67bc4ad318a6f5025adb181b73ffaf3c
unknown <ttg3671@gmail.com>
2026-09-01 18:52:21 +0530

    fix cors headers config corsOption.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -13,5 +13,5 @@ export const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-device-type']
 } 
